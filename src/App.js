5484f4d3bdbc6cc9daa799717502ab09be70407d
netaoren <netaoren8@icloud.com>
2020-04-06 17:03:17 +0300

    create calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState("");
+  const calcBtn = [];
+  const operationsBtn = [];
+  [7, 8, 9, 4, 5, 6, 1, 2, 3, , 0, '.', '%'].forEach((item) => {
+    calcBtn.push(
+        <button
+            onClick={(e) => {
+              setData(data + e.target.value)
+            }}
+            key={item}
+            value={item}>
+          {item}
+        </button>
+    );
+  });
+  ['/', '*', '-', '+'].forEach((operator) => {
+    operationsBtn.push(
+        <button onClick={(e) => {
+          setData(data + e.target.value);
+        }}
+                key={operator}
+                value={operator}>
+          {operator}
+        </button>
+    );
+  });
+  operationsBtn.push(
+      <button
+          value="="
+          key="="
+          onClick={(e) => {
+            try {
+              setData(
+                  String(eval(data)).length > 3 &&
+                  String(eval(data)).includes('.')?
+                      String(eval(data).toFixed(4)) :
+                      String(eval(data))
+              );
+            } catch (err) {
+              console.log(err);
+            }
+          }}>
+        =
+      </button>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className="data">
+          <h4>{data}</h4>
+        </div>
+        <div className="wrapper">
+          <div className="clear">
+            <button onClick={(e) => {
+              setData(d => d.slice(0, data.length - 1));
+            }}
+                    key={"Clear"}>
+              Clear
+            </button>
+          </div>
+          <div className="ac">
+            <button
+                onClick={(e) => {
+                  setData(d => d = "");
+                }}
+                key={"AC"}>
+              AC
+            </button>
+          </div>
+          <div className="digits">
+            {calcBtn}
+          </div>
+          <div className="operations">
+            {operationsBtn}
+          </div>
+        </div>
+      </div>
   );
 }
 
